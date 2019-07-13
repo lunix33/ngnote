3ae2ced8eecc5bfa7a10f12539b93aa479b16448
Patrick Bergeron <pbergeron@lunix.pro>
2019-07-13 15:44:18 -0400

    Made the API base URL based on the origin, or an overide URL stored in LocalStorage.

--- src/app/class/common.ts
+++ src/app/class/common.ts
@@ -1,7 +1,7 @@
 import { Injector } from '@angular/core';
 
 export let InjectorInstance: Injector;
-export const ApiUrl: string = "http://localhost:8080/api";
+export const ApiUrl: string = `${(localStorage.getItem('api')) ? localStorage.getItem('api') : location.origin}/api`;
 
 export function InitInjector(injector: Injector) {
     if (InjectorInstance == null)
