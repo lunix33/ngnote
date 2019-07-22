a9c32da6fffd494d220488d70123725f987a859a
Patrick Bergeron <pat.bergeron@cgi.com>
2019-07-22 14:57:36 -0400

    Removed some content from the home file.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { debounceTime } from 'rxjs/operators'
 
 import { Note } from '../../class/note';
 import { HttpStatusCode } from 'src/app/class/httpstatus';
@@ -13,33 +11,7 @@ import { HttpStatusCode } from 'src/app/class/httpstatus';
 export class HomeComponent {
 	public notes: Note[] = [];
 
-	private _author: string;
-	public get author(): string { return this._author; }
-	public set author(v: string) {
-		this._author = v;
-
-		// Get all the notes to display.
-		Note.GetAll(v)
-			.then((notes: Note[]) => {
-				for (let n of notes) {
-					n.Added = new Date(n.Added).toLocaleString();
-					n.Updated = new Date(n.Updated).toLocaleString();
-				}
-				this.notes = notes
-			})
-			.catch(err => console.error(err));
-	}
-
-	public authorControl: FormControl = new FormControl();
-
 	constructor() {
-		// Get the content of the author field.
-		this.author = localStorage.getItem('author');
-
-		// On author update
-		this.authorControl.valueChanges
-			.pipe(debounceTime(500))
-			.subscribe(this.onAuthorInput.bind(this));
 	}
 
 	/**
@@ -68,14 +40,5 @@ export class HomeComponent {
 				}
 			}
 		}
-	}
-
-	/**
-	 * Event fired when the author field is modified.
-	 * @param v The new value of the field.
-	 */
-	onAuthorInput(v: string) {
-		this.author = v;
-		localStorage.setItem('author', this.author);
 	}
 }
